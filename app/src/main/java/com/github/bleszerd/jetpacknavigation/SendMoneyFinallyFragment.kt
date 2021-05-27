573c391cd94d9a8e39807039b949619115d57246
Vinicius Resende <alive2k@programmer.net>
2021-05-27 15:53:48 -0300

    Button to welcomeFragment

--- app/src/main/java/com/github/bleszerd/jetpacknavigation/SendMoneyFinallyFragment.kt
+++ app/src/main/java/com/github/bleszerd/jetpacknavigation/SendMoneyFinallyFragment.kt
@@ -4,10 +4,14 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.TextView
 import androidx.fragment.app.Fragment
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
 
-class SendMoneyFinallyFragment : Fragment() {
+class SendMoneyFinallyFragment : Fragment(), View.OnClickListener {
+    lateinit var navController: NavController
     lateinit var recipient: String
     lateinit var money: Money
 
@@ -19,6 +23,14 @@ class SendMoneyFinallyFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_send_money_finally, container, false)
     }
 
+    override fun onClick(v: View?) {
+        val buttonPressedId = v?.id
+
+        if (buttonPressedId != null) {
+            navController.navigate(R.id.action_sendMoneyFinallyFragment_to_welcomeFragment)
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         recipient = arguments?.getString("recipient").toString()
@@ -28,7 +40,9 @@ class SendMoneyFinallyFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        navController = Navigation.findNavController(view)
         val amount = money.amount
+        view.findViewById<Button>(R.id.back_to_welcome_button).setOnClickListener(this)
         view.findViewById<TextView>(R.id.send_money_text_view).text = "Sending $amount to"
         view.findViewById<TextView>(R.id.person_name_text_view).text = recipient
     }
